09b68281addbc68f5b955093c8b615612e6a7e4b
Welber Serafim <serafimwelber@gmail.com>
2020-12-21 20:53:51 -0300

    Commit incial do projeto

---
+++ src/main/java/br/com/agenteDaCidade/AgenteDaCidadeApplication.java
@@ -0,0 +1,13 @@
+package br.com.agenteDaCidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenteDaCidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenteDaCidadeApplication.class, args);
+	}
+
+}
